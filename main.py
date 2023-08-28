6a74c9bc9bd8f5beae6ebc3c8e9b8462d0b83018
salahawk <bstuqopavyrwku8251@gmail.com>
2023-08-28 10:55:39 -0700

    feat: proxy authentication

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import asyncio
+import base64
+import hashlib
+import hmac
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
+EXPECTED_USERNAME = os.getenv("PROXY_USERNAME")
+EXPECTED_PASSWORD = os.getenv("PROXY_PASSWORD")
+EXPECTED_USERNAME_HASH = hashlib.sha512(EXPECTED_USERNAME.encode()).digest()
+EXPECTED_PASSWORD_HASH = hashlib.sha512(EXPECTED_PASSWORD.encode()).digest()
+
+
+def parse_basic_auth(auth_str):
+    prefix = b"Basic "
+    if not auth_str.startswith(prefix):
+        return "", "", False
+    try:
+        decoded_str = base64.b64decode(auth_str[len(prefix) :]).decode()
+        username, password = decoded_str.split(":")
+        return username, password, True
+    except Exception:
+        return "", "", False
+
+
+def handle_authentication(auth_header):
+    username, password, ok = parse_basic_auth(auth_header)
+    if not ok:
+        return False
+    username_hash = hashlib.sha512(username.encode()).digest()
+    password_hash = hashlib.sha512(password.encode()).digest()
+    equal_username = hmac.compare_digest(username_hash, EXPECTED_USERNAME_HASH)
+    equal_password = hmac.compare_digest(password_hash, EXPECTED_PASSWORD_HASH)
+    return equal_username and equal_password
 
 
 async def handle_client(reader, writer):
@@ -14,11 +46,26 @@ async def handle_client(reader, writer):
         target_host, target_port = target_host.split(b":")
         target_port = int(target_port)
 
-        logger.info(f"coming request: {target_host}:{target_port}")
+        # Extract the Proxy-Authorization header from the request
+        auth_header = next(
+            (
+                line.split(b": ", 1)[1]
+                for line in data.split(b"\r\n")
+                if line.lower().startswith(b"proxy-authorization: ")
+            ),
+            None,
+        )
+        if not handle_authentication(auth_header):
+            writer.write(b"HTTP/1.1 407 Proxy Authentication Required\r\n\r\n")
+            await writer.drain()
+            writer.close()
+            return
+
+        logger.info(f"coming request: {target_host.decode()}:{target_port}")
 
         try:
             target_reader, target_writer = await asyncio.open_connection(
-                target_host, target_port
+                target_host.decode(), target_port
             )
 
             writer.write(b"HTTP/1.1 200 Connection Established\r\n\r\n")
@@ -36,7 +83,7 @@ async def handle_client(reader, writer):
                 forward(reader, target_writer), forward(target_reader, writer)
             )
         except Exception as e:
-            print(f"Error: {e}")
+            logger.error(f"Error: {e}")
 
         writer.close()
     else:
@@ -49,7 +96,7 @@ async def main():
 
     server = await asyncio.start_server(handle_client, bind_ip, bind_port)
 
-    print(f"[*] Listening on {bind_ip}:{bind_port}")
+    logger.info(f"[*] Listening on {bind_ip}:{bind_port}")
 
     async with server:
         await server.serve_forever()
