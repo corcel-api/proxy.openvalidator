c7437e3e8c42af2574b0a54e13b4e04db9deab58
salahawk <bstuqopavyrwku8251@gmail.com>
2023-10-10 10:22:55 -0700

    feat: error handling

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ logging.basicConfig(level=logging.INFO)
 
 EXPECTED_USERNAME = os.getenv("PROXY_USERNAME")
 EXPECTED_PASSWORD = os.getenv("PROXY_PASSWORD")
-EXPECTED_USERNAME_HASH = hashlib.sha512(EXPECTED_USERNAME.encode()).digest()
-EXPECTED_PASSWORD_HASH = hashlib.sha512(EXPECTED_PASSWORD.encode()).digest()
+EXPECTED_USERNAME_HASH = (
+    hashlib.sha512(EXPECTED_USERNAME.encode()).digest() if EXPECTED_USERNAME else None
+)
+EXPECTED_PASSWORD_HASH = (
+    hashlib.sha512(EXPECTED_PASSWORD.encode()).digest() if EXPECTED_PASSWORD else None
+)
 
-# Create a global counter for request IDs
 request_id_counter = itertools.count()
 
 
@@ -51,6 +54,8 @@ def parse_basic_auth(auth_str):
 
 
 def handle_authentication(auth_header):
+    if not EXPECTED_USERNAME or not EXPECTED_PASSWORD:
+        return True
     username, password, ok = parse_basic_auth(auth_header)
     if not ok:
         return False
@@ -65,81 +70,88 @@ async def handle_client(reader, writer):
     request_id = next(request_id_counter)
     logger.info(f"{request_id}: New request")
 
-    data = await reader.read(4096)
-    first_line = data.split(b"\n")[0]
-    method, target_host, _ = first_line.split(b" ")
-
-    # Check if authentication is required
-    require_auth = EXPECTED_USERNAME is not None and EXPECTED_PASSWORD is not None
-    # Extract the Proxy-Authorization header from the request
-    auth_header = next(
-        (
-            line.split(b": ", 1)[1]
-            for line in data.split(b"\r\n")
-            if line.lower().startswith(b"proxy-authorization: ")
-        ),
-        None,
-    )
-    if require_auth and not handle_authentication(auth_header):
-        logger.info(f"{request_id}: Authentication failed")
-        writer.write(
-            b"HTTP/1.1 407 Proxy Authentication Required\r\nProxy-Authenticate: Basic\r\n\r\n"
-        )
-        await writer.drain()
-        writer.close()
-        return
-
-    if method != b"CONNECT":
-        # Handle non-CONNECT methods here
-        async with aiohttp.ClientSession() as session:
-            async with session.request(method.decode(), target_host.decode()) as resp:
-                writer.write(f"HTTP/1.1 {resp.status} {resp.reason}\r\n".encode())
-                headers = remove_hop_headers(resp.headers)
-                for header, value in headers.items():
-                    writer.write(f"{header}: {value}\r\n".encode())
-                writer.write(b"\r\n")
-                await writer.drain()
-                body = await resp.read()
-                writer.write(body)
-                await writer.drain()
-
-        writer.close()
-        return
-
-    target_host, target_port = target_host.split(b":")
-    target_port = int(target_port)
-
-    logger.info(f"coming request: {target_host.decode()}:{target_port}")
-
-    target_reader, target_writer = await asyncio.open_connection(
-        target_host.decode(), target_port
-    )
     try:
-        writer.write(b"HTTP/1.1 200 Connection Established\r\n\r\n")
-        await writer.drain()
+        data = await reader.read(4096)
+        first_line = data.split(b"\n")[0]
+        method, target_host, _ = first_line.split(b" ")
 
-        async def forward(src, dst):
-            while True:
-                data = await src.read(4096)
-                if len(data) == 0:
-                    dst.close()
-                    break
-                dst.write(data)
-                await dst.drain()
-
-        await asyncio.gather(
-            forward(reader, target_writer),
-            forward(target_reader, writer),
+        auth_header = next(
+            (
+                line.split(b": ", 1)[1]
+                for line in data.split(b"\r\n")
+                if line.lower().startswith(b"proxy-authorization: ")
+            ),
+            None,
         )
+        if not handle_authentication(auth_header):
+            logger.info(f"{request_id}: Authentication failed")
+            writer.write(
+                b"HTTP/1.1 407 Proxy Authentication Required\r\nProxy-Authenticate: Basic\r\n\r\n"
+            )
+            await writer.drain()
+            writer.close()
+            return
+
+        if method != b"CONNECT":
+            async with aiohttp.ClientSession() as session:
+                async with session.request(
+                    method.decode(), target_host.decode()
+                ) as resp:
+                    writer.write(f"HTTP/1.1 {resp.status} {resp.reason}\r\n".encode())
+                    headers = remove_hop_headers(resp.headers)
+                    for header, value in headers.items():
+                        writer.write(f"{header}: {value}\r\n".encode())
+                    writer.write(b"\r\n")
+                    await writer.drain()
+                    body = await resp.read()
+                    writer.write(body)
+                    await writer.drain()
+        else:
+            target_host, target_port = target_host.split(b":")
+            target_port = int(target_port)
+
+            logger.info(
+                f"{request_id} coming request: {target_host.decode()}:{target_port}"
+            )
+
+            try:
+                target_reader, target_writer = await asyncio.open_connection(
+                    target_host.decode(), target_port
+                )
+            except Exception as e:
+                logger.error(f"{request_id} Error: {e}")
+                return
+
+            writer.write(b"HTTP/1.1 200 Connection Established\r\n\r\n")
+            await writer.drain()
+
+            async def forward(src, dst):
+                while True:
+                    data = await src.read(4096)
+                    if len(data) == 0:
+                        dst.close()
+                        break
+                    try:
+                        dst.write(data)
+                        await dst.drain()
+                    except Exception as e:
+                        logger.error(f"{request_id} Error: {e}")
+                        break
+
+            await asyncio.gather(
+                forward(reader, target_writer),
+                forward(target_reader, writer),
+            )
     except Exception as e:
-        logger.error(f"Error: {e}")
+        logger.error(f"{request_id} Error: {e}")
     finally:
         writer.close()
-        target_writer.close()
 
-    # After closing the connections, discard any remaining data
-    while await reader.read(4096):
-        pass
+    try:
+        while await reader.read(4096):
+            pass
+    except Exception as e:
+        logger.error(f"{request_id} Error: {e}")
 
     logger.info(f"{request_id}: Request done")
 
